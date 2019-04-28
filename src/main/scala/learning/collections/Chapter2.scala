0617dd2f0664f1c73fa6b29489ce73e86db06526
400 <xuxianhong91@gmail.com>
2019-04-28 09:28:51 +0800

    c2

--- src/main/scala/learning/collections/Chapter2.scala
+++ src/main/scala/learning/collections/Chapter2.scala
@@ -2,4 +2,30 @@ package learning.collections
 
 object Chapter2 {
 
+  // 不能简单的new
+  //val pet = new Pet()
+
+  val t = List(1,2,3).toTraversable
+  t
+
+
+  val s = Traversable(1,2,3)
+
+
+  trait Pet{
+    def sayHello(name:String): String ={
+      s"hello $name"
+    }
+  }
+
+  object Pet{
+    def apply(name:String): Pet = new Pet{
+
+    }
+  }
+
+
 }
+
+
+
